9771ed17d3b8a16c6e62c2ba7abf59174d5f9233
Martin Finke <martin@martin-finke.de>
2017-10-24 18:16:55 +0200

    Removed varx_LockFreeSource.cpp

--- varx/varx.cpp
+++ varx/varx.cpp
@@ -9,6 +9,5 @@ using namespace juce;
 
 #include "util/internal/varx_any.cpp"
 #include "util/internal/varx_ReleasePool.cpp"
-#include "util/varx_LockFreeSource.cpp"
 #include "util/varx_PrintFunctions.cpp"
 }
