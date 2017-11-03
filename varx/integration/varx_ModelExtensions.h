cfb65d6d8a0d21ed818435c7dc9e902317a6e4cb
Martin Finke <martin@martin-finke.de>
2017-11-03 09:50:03 +0100

    Fixed comments, added JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR

--- varx/integration/varx_ModelExtensions.h
+++ varx/integration/varx_ModelExtensions.h
@@ -1,9 +1,9 @@
 #pragma once
 
 /**
- Connects a juce::Value with a BehaviorSubject.
+ Connects a `juce::Value` with a `BehaviorSubject`.
  
- Whenever the Value changes, the BehaviorSubject is changed, and vice versa.
+ Whenever the `Value` changes, the `BehaviorSubject` is changed, and vice versa.
  */
 class ValueExtension : private juce::Value::Listener
 {
@@ -19,10 +19,14 @@ private:
     DisposeBag disposeBag;
     
     void valueChanged(juce::Value&) override;
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ValueExtension);
 };
 
 /**
-    Adds reactive extensions to a juce::AudioProcessor.
+ Adds reactive extensions to an `AudioProcessor`.
+ 
+ If you use this directly (instead of `Reactive<AudioProcessor>`), you **must** make sure that the `AudioProcessor` has a longer lifetime than this `AudioProcessorExtension`!
  */
 class AudioProcessorExtension : private juce::AudioProcessorListener
 {
@@ -43,10 +47,14 @@ private:
     
     void audioProcessorParameterChanged(juce::AudioProcessor*, int, float) override {}
     void audioProcessorChanged(juce::AudioProcessor*) override;
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioProcessorExtension);
 };
 
 /**
- Adds reactive extensions to a juce::AudioProcessorValueTreeState.
+ Adds reactive extensions to an `AudioProcessorValueTreeState`.
+ 
+ If you use this directly (instead of `Reactive<AudioProcessorValueTreeState>`), you **must** make sure that the `AudioProcessorValueTreeState` has a longer lifetime than this `AudioProcessorValueTreeStateExtension`!
  */
 class AudioProcessorValueTreeStateExtension {
 public:
@@ -58,7 +66,7 @@ public:
     /**
      Returns a subject to control the value of the parameter with the given ID.
      
-     If this is called early in the app lifecycle, the subject contains var(), and not the parameter's default value. This is because JUCE updates the ValueTree asynchronously.
+     If this is called early in the app lifecycle, the subject contains `var()` for a short amount of time, and not the parameter's default value. This is because JUCE updates the ValueTree asynchronously.
      
      Parameter values can be changed from the audio thread; in this case the subject's Observable side emits asynchronously.
      */
@@ -69,4 +77,6 @@ private:
     const juce::ScopedPointer<Impl> impl;
     juce::AudioProcessorValueTreeState& parent;
     DisposeBag disposeBag;
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioProcessorValueTreeStateExtension);
 };
