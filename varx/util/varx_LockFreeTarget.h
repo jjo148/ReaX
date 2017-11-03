f10577990dd3021076001c223517b5a282d73ee1
Martin Finke <martin@martin-finke.de>
2017-11-03 11:00:03 +0100

    Replaced LockFreeTarget by queue-based implementation

--- varx/util/varx_LockFreeTarget.h
+++ varx/util/varx_LockFreeTarget.h
@@ -5,132 +5,67 @@ template<typename T>
 class LockFreeTargetBase
 {
 protected:
+    LockFreeTargetBase()
+    {
+        subject.subscribe([this](const T& newValue) {
+                   queue.enqueue(newValue);
+               })
+            .disposedBy(disposeBag);
+    }
+
+    moodycamel::ConcurrentQueue<T> queue;
     PublishSubject<T> subject;
     DisposeBag disposeBag;
 };
-
-#if __cpp_lib_atomic_is_always_lock_free
-// std::conjunction implementation, because std::conjunction may not be available
-template<class...>
-struct conjunction : std::true_type
-{
-};
-template<class B1>
-struct conjunction<B1> : B1
-{
-};
-template<class B1, class... Bn>
-struct conjunction<B1, Bn...>
-: std::conditional_t<bool(B1::value), conjunction<Bn...>, B1>
-{
-};
-
-template<typename T>
-struct IsLockFree_Impl
-: std::integral_constant<bool, std::atomic<T>::is_always_lock_free>
-{
-};
-
-// Enable juce::Atomic for trivially copyable types that are always lock-free:
-template<typename T>
-using IsLockFree = typename std::enable_if<conjunction<std::is_trivially_copyable<T>, IsLockFree_Impl<T>>::value>::type;
-
-#else
-// Enable juce::Atomic for arithmetic types:
-template<typename T>
-using IsLockFree = typename std::enable_if<std::is_arithmetic<T>::value>::type;
-#endif
 }
 
 /**
- An Observer that stores the latest retrieved item atomically, so it can be read by a different thread.
+ An `Observer` that puts all retrieved values in a lock-free queue. The queue can be accessed from another thread without locking.
  
- In most cases, this should be used with BehaviorSubject, or Observables that immediately emit at least one item when subscribing. This ensures that getValue() isn't called before any items have been retrieved.
- 
- From C++17 onwards, it uses juce::Atomic internally if T is trivially copyable and always lock-free. Before C++17, it uses juce::Atomic for arithmetic types. Otherwise, it uses std::shared_ptr and a ReleasePool to store the retrieved item.
+ Useful to transfer data from a non-realtime thread to a realtime thread.
  */
-template<typename T, typename Enable = void>
+template<typename T>
 class LockFreeTarget : private detail::LockFreeTargetBase<T>, public Observer<T>
 {
 public:
-    /// Creates a new instance. You must not call getValue() before retrieving items.
     LockFreeTarget()
     : Observer<T>(detail::LockFreeTargetBase<T>::subject)
-    {
-        const auto onNext = [this](const T& newValue) {
-            // Copy value to a shared_ptr
-            const auto latest = std::make_shared<const T>(newValue);
-            
-            detail::ReleasePool::get().add(latest);
-            std::atomic_store(&latestValue, latest);
-        };
+    {}
 
-        detail::LockFreeTargetBase<T>::subject.subscribe(onNext).disposedBy(detail::LockFreeTargetBase<T>::disposeBag);
+    /**
+     Dequeues the next value from the queue (if it's non-empty) and assigns it to `value`.
+     
+     Returns `true` iff the queue was non-empty. If it returns `false`, then `value` is untouched.
+     
+     Does not lock. Uses move-assignment if `value` supports it, copy-assignment otherwise. Does not allocate dynamic memory, unless `value` does during assigment.
+     
+     May be called from any thread, including the thread on which the `Observer` retrieves values.
+     */
+    template<typename U>
+    inline bool tryDequeue(U& value)
+    {
+        return detail::LockFreeTargetBase<T>::queue.try_dequeue(value);
     }
 
     /**
-     Reads the latest retrieved item atomically. This can be called from the audio thread (or some other realtime thread). **Must not be called before any item has been retrieved.**
+     Dequeues all values from the queue (if it's non-empty) and assigns the last (newest) value to `value`.
      
-     It returns a copy of the item. If copying is expensive, feel free to use `getValuePointer()` instead of this.
+     Returns `true` iff the queue was non-empty. If it returns `false`, then `value` is untouched.
+     
+     Does not lock. Uses move-assignment if `value` supports it, copy-assignment otherwise. Does not allocate dynamic memory, unless `value` does during assigment.
+     
+     May be called from any thread, including the thread on which the `Observer` retrieves values.
      */
-    inline T getValue() const
+    template<typename U>
+    inline bool tryDequeueAll(U& value)
     {
-        const auto latest = getValuePointer();
+        bool hadValues = false;
+        while (tryDequeue(value))
+            hadValues = true;
 
-        // Must retrieve at least one item before this is called!
-        jassert(latest);
-
-        return *latest;
-    }
-    
-    /**
-     Returns a pointer to the latest value.
-     
-     The value in the returned pointer never changes, it is a snapshot of the current state.
-     
-     Unlike `getValue()`, this does not make a copy of the item. So it should be preferred if copying the item is expensive.
-     */
-    inline std::shared_ptr<const T> getValuePointer() const
-    {
-        return std::atomic_load(&latestValue);
+        return hadValues;
     }
 
 private:
-    std::shared_ptr<const T> latestValue;
-
-    JUCE_LEAK_DETECTOR(LockFreeTarget)
-};
-
-/**
- An Observer that stores the latest retrieved item atomically, so it can be read by a different thread.
- 
- In most cases, this should be used with BehaviorSubject, or Observables that immediately emit at least one item when subscribing. This ensures that getValue() isn't called before any items have been retrieved.
- 
- From C++17 onwards, it uses juce::Atomic internally if T is trivially copyable and always lock-free. Before C++17, it uses juce::Atomic for arithmetic types. Otherwise, it uses std::shared_ptr and a ReleasePool to store the retrieved item.
- */
-template<typename T>
-class LockFreeTarget<T, detail::IsLockFree<T>> : private detail::LockFreeTargetBase<T>, public Observer<T>
-{
-public:
-    /// Creates a new instance. Before retrieving items, getValue() returns an unitialized value.
-    LockFreeTarget()
-    : Observer<T>(detail::LockFreeTargetBase<T>::subject)
-    {
-        const auto onNext = [this](const T& newValue) {
-            latestValue.set(newValue);
-        };
-        
-        detail::LockFreeTargetBase<T>::subject.subscribe(onNext).disposedBy(detail::LockFreeTargetBase<T>::disposeBag);
-    }
-
-    /// Reads the latest retrieved item atomically. This can be called from the audio thread (or some other realtime thread). If this is called before any item has been retrieved, the returned value is uninitialized.
-    inline T getValue() const
-    {
-        return latestValue.get();
-    }
-
-private:
-    juce::Atomic<T> latestValue;
-
-    JUCE_LEAK_DETECTOR(LockFreeTarget)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(LockFreeTarget)
 };
